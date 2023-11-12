a8b08aa73ffecd4637cef7f09483e2a8f5ed52cf
Ievgen Garkusha <eugengarkusha@gmail.com>
2023-11-12 11:20:02 +0100

    wip

--- history.js
+++ history.js
@@ -5,40 +5,36 @@ const path = require("path");
 module.exports = {
   getDiff (prev, next)  {
     console.log("prev="+ JSON.stringify(prev))
-    const coutries = [...Object.keys(prev), ...Object.keys(next)];
-    return coutries
+    const countries = [...Object.keys(prev), ...Object.keys(next)];
+    return countries
       .map((country) => {
         const prevVal = prev[country];
         const nextVal = next[country];
 
-        return !_.isEqual(nextVal, prevVal) && { country, prevVal, nextVal };
+        return !_.isEqual(nextVal, prevVal) && { country, prev: prevVal, next: nextVal };
       })
       .filter(Boolean);
   },
-  getRelevantDiffEntries(diff)  {
-   console.log("diff="+JSON.stringify(diff))
-    const entries = diff.map(({ country, prev, next }) => [
-      country,
-      // picking next if prev is not defined(country added), next is not defined(country deleted) or prev is failed to fetch
-        // TODO: when next is undef, it is not being saved to obj and cant serve as  as tombstone/ need special value
-      next?.kind === "failed_to_fetch" ? prev : next,
-    ]);
-    console.log("entries="+JSON.stringify(entries))
-    return Object.fromEntries(entries);
-  },
+
   // walk the history back in time and recover from failed_to_fetch values if possible
-  // history is array of fetched prices ordered by fetch time desc
+  // history is array of price-fetch-result objects ordered by fetch time desc
   mergeHistory(history) {
-    let res;
-    if (history.size === 0) return [];
+    let res = history[0];
+    if (!res) return {};
     else {
-      res = history[0];
+      let failedToFetchCountries = Object.entries(res).flatMap(([c, v]) => v.kind === "failed_to_fetch" && c )
       for (const h of _.tail(history)) {
-        res = { ...res, ...self.getRelevantDiffEntries(self.getDiff(h, res)) };
+          for (const c of failedToFetchCountries) {
+              const priceObj = h[c]
+              if (priceObj && priceObj?.kind !== "failed_to_fetch") {
+                  res[c] = priceObj
+                  failedToFetchCountries = failedToFetchCountries.filter(cc => cc !== c)
+              }
+          }
       }
-      console.log("res="+JSON.stringify(res))
-      return res;
     }
+
+      return res;
   },
   buildFileDir(basePath, date)  {
       return `${basePath}/results/${date.getUTCFullYear()}/${date.getUTCMonth()}`
@@ -57,6 +53,5 @@ module.exports = {
     }
     return results;
   },
-
 };
 self = module.exports;
